a8a2393529c1b6a1af9a8664f9d789efa0e8af98
Tom Studer <tom@tomstuder.com>
2013-08-14 21:55:55 -0400

    added users to dropdown

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #attr_accessible :title, :body
+
+  #Setup accessible (or protected) attributes for your model
   attr_accessible :first_name, :last_name, :email, :profile_name, :password, :password_confirmation, :remember_me
+ 
+
+ 
+   #attr_accessible :title, :body
+   has_many :statuses
+  
+  def full_name
+  first_name + " " + last_name
+  end
 end
